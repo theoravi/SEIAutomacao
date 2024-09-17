2d6cff17904c14bca2d03b9f708a771d1589fbc4
andrejestagio <andrej.estagio@anatel.gov.br>
2024-09-17 13:28:21 -0300

    fix: mudanças no teste.py

--- teste.py
+++ teste.py
@@ -2,11 +2,13 @@
 import pandas as pd
 from tabulate import tabulate
 import numpy as np
+import funcoes as fc
 
-preenchido = {'Modelo': ["MT3PD\n", "RM 330"], 'Nome Comercial': ["MINI 3", "C5"], 'Número de Série (incluindo rádio controle e óculos)': ["1581FFGDF564GFDRA45", "5HAZ54GDGDG6"] }
+preenchido = {'Modelo': [np.nan, "RM330"], 'Nome Comercial': ["MINI 3", "C5"], 'Número de Série (incluindo rádio controle e óculos)': ["1581FFGDF564GFDRA45", "5HAZ54GDGDG6"] }
 df = pd.DataFrame(preenchido)
 modelos = df['Modelo']
 modelos = modelos.reset_index(drop=True)
+modelos = modelos.dropna()
 print(df)
 print(modelos)
 print("\n")
@@ -16,26 +18,52 @@ data = df.values.tolist()
 headers = df.columns.tolist()
 print(tabulate(data, headers=headers, tablefmt='pretty'))
 print('\n')
-tabela = pd.read_excel("C:\\Users\\andrej.estagio\\ANATEL\\ORCN - Drones\\Lista de Drones Anatel_Corrigida.xlsx")
-tabela.columns = tabela.iloc[1]
-tabela = tabela.iloc[2:]
-tabela = tabela.reset_index(drop=True)
+planilhaDrones = input("Insira o caminho da planilha/lista de drones conformes: ")
+#RETIRA ASPAS CASO EXISTA NO CAMINHO DA PLANILHA
+planilhaDrones = planilhaDrones.replace('"' , '')
+tabela = fc.corrige_planilha(planilhaDrones)
 tabela = tabela['MODELO']
-print(modelos[1])
-print(type(modelos[1]))
-print(modelos[1].lower())
 # checkexcel = modelos.isin(tabela['MODELO'])
 # checkexcel = tabela['MODELO'].isin(modelos)
-input("Pressione qualquer tecla para continuar")
+    
 checkexcel = []
-for i in tabela:
-    for j in range(len(modelos)):
-        if modelos[j].lower() == i.lower():
-            checkexcel[j] = True
+for i in modelos:
+    checkexcel.append(0)
+for modelo_solicitante in range(len(modelos)):
+    j = 0
+    while j < len(tabela):
+        if modelos[modelo_solicitante].lower() == str(tabela[j]).lower():
+            checkexcel[modelo_solicitante] = True
+            break
+        else:
+            checkexcel[modelo_solicitante] = False
+            j+=1
+
+# for i in tabela:
+#     for j in range(len(modelos)):
+#         if modelos[j].lower() == str(i).lower():
+#             checkexcel[j] = True
+#             print("modelo é igual")
+#         else:
+#             if not checkexcel[j]:
+#                 checkexcel[j] = False
+# for i in range(len(tabela)):
+#     tabela[i] = str(tabela[i])
+#     tabela[i] = tabela[i].lower()
+    
+
+# for modelo in modelos:
+#     if modelo.lower() in tabela:
+#         print(f"O modelo {modelo} está na lista de drones conformes.")
+#     else:
+#         print(f"O modelo {modelo} não se encontra na lista de drones conformes")
+
+
+            
 print(checkexcel)
 input()
 for i in range(len(checkexcel)):
-    if checkexcel[i] == True:
+    if checkexcel[i]:
         print(f"O modelo {modelos[i]} está na lista de drones conformes.")
     else:
         print(f"O modelo {modelos[i]} não se encontra na lista de drones conformes")
