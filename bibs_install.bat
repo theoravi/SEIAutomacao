b03757cabb0af3f2534f5924ffb38bbf9264a3b0
andrejestagio <andrej.estagio@anatel.gov.br>
2024-09-27 12:38:45 -0300

    accept: comentarios no arquivo .bat

--- bibs_install.bat
+++ bibs_install.bat
@@ -3,12 +3,9 @@ pip install pandas
 pip install pyautogui
 pip install pyperclip
 pip install undetected-chromedriver
-<<<<<<< HEAD
-=======
 :: A partir dar versão 3.12 do Python, "distulus" foi removido e é necessário na biblioteca "undetected-chromedriver".
 :: Para funcionar, é necessário instalar a biblioteca "setuptools" que contém "distulus".
 pip install setuptools
->>>>>>> branchLukas
 pip install unicode
 pip install selenium
 pip install tabulate
